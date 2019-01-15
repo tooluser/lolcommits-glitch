5b5b7566ad7887650b73106ca95cdccea770ac5f
Joshua Marker <jmarker@usertesting.com>
2019-01-14 16:14:57 -0800

    Refactor image glitching into image

--- lib/lolcommits/plugin/glitch.rb
+++ lib/lolcommits/plugin/glitch.rb
@@ -45,12 +45,37 @@ module Lolcommits
         debug "In Image: #{in_image.inspect}"
         debug "Glitching…"
         
-        out_image = in_image.dup
+        glitch_image = glitch_image(in_image)
+
+        glitch_image.write runner.main_image
+      end
+
+      def enabled?
+        super
+      end
+
+      def configure_options!
+        super
+      end
+
+      def default_options
+        {
+          glitch_level: 7,
+        }
+      end
+
+      def valid_configuration?
+        @configuration != nil && @configuration[:glitch_level] != nil
+      end
+
+      private
+
+      private def glitch_image(image)
+        glitch_image = image.dup
+        debug "Out Image: #{glitch_image.inspect}"
         
-        debug "Out Image: #{out_image.inspect}"
-        
-        width = in_image.columns
-        height = in_image.rows
+        width = image.columns
+        height = image.rows
         
         debug "Glitching #{width} x #{height} #{config_option(:glitch_level)} times"
         
@@ -62,85 +87,17 @@ module Lolcommits
 
           case rand(1..4)
           when 1
-            out_image.composite!(in_image, start_x, start_y, COMPOSITE_OPERATORS.sample)
+            glitch_image.composite!(image, start_x, start_y, COMPOSITE_OPERATORS.sample)
           when 2
-            out_image.composite!(in_image, -start_x, start_y, COMPOSITE_OPERATORS.sample)
+            glitch_image.composite!(image, -start_x, start_y, COMPOSITE_OPERATORS.sample)
           when 3
-            out_image.composite!(in_image, start_x, -start_y, COMPOSITE_OPERATORS.sample)
+            glitch_image.composite!(image, start_x, -start_y, COMPOSITE_OPERATORS.sample)
           when 4
-            out_image.composite!(in_image, -start_x, -start_y, COMPOSITE_OPERATORS.sample)
+            glitch_image.composite!(image, -start_x, -start_y, COMPOSITE_OPERATORS.sample)
           end
         end
 
-        out_image.write runner.main_image
-      end
-
-      def enabled?
-        super
-      end
-
-      ##
-      # Prompts the user to configure all plugin options.
-      #
-      # Available options can be defined in an Array (@options instance var)
-      # and/or a Hash (by overriding the `default_options` method).
-      #
-      # By default (on initialize), `@options` is set with `[:enabled]`. This is
-      # mandatory since `enabled?` checks this option is true before running any
-      # capture hooks.
-      #
-      # Using a Hash to define default options allows you to:
-      #
-      #  - fall back to default values
-      #  - define nested options, user is prompted for each nested option key
-      #
-      # `configure_option_hash` will iterate over all options prompting the user
-      # for input and building the configuration Hash.
-      #
-      # Lolcommits will save this Hash to a YAML file. During the capture
-      # process the configuration is loaded, parsed and available in the plugin
-      # class as `@configuration`. Or if you want to fall back to default
-      # values, you should use `config_option` to dig the hash.
-      #
-      # Alternatively you can override this method entirely to customise the
-      # process. A helpful `parse_user_input` method is available to help parse
-      # strings from STDIN (into boolean, integer or nil values).
-      #
-      # @return [Hash] the configured plugin options
-      #
-      def configure_options!
-        super
-      end
-
-      ##
-      # Returns a hash of default options to be presented to the user when
-      # configuring (with `configure_options!`.
-      #
-      # @return [Hash] with the default option names (keys) and values
-      #
-      def default_options
-        {
-          glitch_level: 7,
-        }
-      end
-
-      ##
-      # Returns true/false indicating if the plugin has been correctly
-      # configured.
-      #
-      # The default superclass method simply checks if `@configuration` is
-      # present (not empty).
-      #
-      # By default if this method returns false, plugin hooks will not execute
-      # and a warning message is shown prompting the user to re-configure the
-      # plugin.
-      #
-      # Override to define your own configuration checks and/or messaging.
-      #
-      # @return [Boolean] true/false indicating if plugin is correct configured
-      #
-      def valid_configuration?
-        @configuration != nil && @configuration[:glitch_level] != nil
+        glitch_image
       end
     end
   end
